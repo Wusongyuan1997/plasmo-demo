58e61ac9e28142b7a40d3b44b0d4a91f21228b71
wusongyuan <wusongyuan@zbj.com>
2022-08-30 18:02:48 +0800

    fix: test

--- popup.tsx
+++ popup.tsx
@@ -11,7 +11,7 @@ function IndexPopup() {
         padding: 16
       }}>
       <h2>
-        Welcome to your{" "}
+        Welcome to your11111{" "}
         <a href="https://www.plasmo.com" target="_blank">
           Plasmo
         </a>{" "}
